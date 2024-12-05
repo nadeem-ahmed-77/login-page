21aead73dc2f856cbe2fa67c5b3216ba29b37ccc
Nadeem Ahmed <nz110703@gmail.com>
2024-12-05 16:58:14 +0500

    first commit

---
+++ pages/deshboard/deshboard.js
@@ -0,0 +1,9 @@
+
+function submitForm(e){
+    e.preventDefault();
+  
+  
+  
+  
+    
+  }
---
+++ pages/login/login.js
@@ -0,0 +1,28 @@
+
+var usersData = JSON.parse(localStorage.getItem('users'));
+
+
+function searchData(user) {
+  for (let i = 0; i < usersData.length; i++) {
+    if(user.password === usersData[i].password && user.email === usersData[i].email){
+      return usersData[i];
+    }
+  }
+  
+}
+
+function submitForm(e){
+  e.preventDefault();
+  var passValue = document.getElementById("password").value;
+  var emailValue = document.getElementById("email").value;
+
+  var isLogin = searchData({password: passValue,email: emailValue,})
+  if(isLogin){
+    console.log(isLogin)
+    localStorage.setItem('login',JSON.stringify(isLogin));
+    window.location.replace('../deshboard/deshboard.html')
+  }
+
+
+  
+}
